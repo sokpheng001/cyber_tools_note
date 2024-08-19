fd0b86954d1489fd582f45d70341e7f890ca5fda
1-OTP <piu9066@gmail.com>
2024-08-19 23:41:46 +0700

    update

--- auto-push.sh
+++ auto-push.sh
@@ -1,11 +1,11 @@
 git add .
-echo "Commit message: "
+echo -n "Commit message: "
 read user_message
 git commit -m "$user_message"
 
 
 echo "Pushing changes to remote repository..."
-echo "Branch name: "
+echo -n "Branch name: "
 read branch_name
 git push origin "$branch_name"
 
