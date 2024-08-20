77d6c4534bc0c37b22f04765f1c0b58e4051f714
sokpheng001 <126130178+sokpheng001@users.noreply.github.com>
2024-08-20 21:59:41 +0700

    Update set-to-private-repo.sh

--- set-to-private-repo.sh
+++ set-to-private-repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TOKEN="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"
-USERNAME="sokpheng001"
+TOKEN="yourgithub_token"
+USERNAME="github_username"
 
 # List all repositories
 repos=$(gh repo list $USERNAME --limit 100 --json name -q '.[].name')
